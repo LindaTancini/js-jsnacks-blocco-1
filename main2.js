72963c3635edc303627fbd8af8f36772fe22c840
LindaTancini <lindatancini95@gmail.com>
2025-02-27 12:31:07 +0100

    feat: add js snack-2

--- main2.js
+++ main2.js
@@ -1,3 +1,22 @@
 //jsnack2
 //L’utente inserisce due parole in successione, con due prompt.
 //Il software stampa prima la parola più corta, poi la parola più lunga.
+
+// inserisco prima parola
+const firstWord = prompt("Inserisci la prima parola");
+console.log(firstWord);
+// inserisco seconda parola
+const secondWord = prompt("Inserisci la seconda parola");
+console.log(secondWord);
+
+//  vedere quale parola è più corta e più lunga
+
+if (firstWord.length < secondWord.length) {
+  console.log("La parola più corta è= " + firstWord);
+  console.log("La parola più lunga è= " + secondWord);
+} else if (secondWord.length < firstWord.length) {
+  console.log("La parola più lunga è= " + firstWord);
+  console.log("La parola più corta è= " + secondWord);
+} else {
+  console.log("Le parole sono lunghe uguali= " + firstWord + secondWord);
+}
