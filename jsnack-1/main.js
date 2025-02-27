2751d13a4f28a4c5b8c521589ccdb0a735e6bdda
LindaTancini <lindatancini95@gmail.com>
2025-02-27 12:19:38 +0100

    feat: add js snack-1

--- jsnack-1/main.js
+++ jsnack-1/main.js
@@ -1,3 +1,20 @@
 // jsnack1
 //L’utente inserisce due numeri in successione, con due prompt.
 //Il software stampa il maggiore.
+
+// primo numero da inserire
+const num1 = parseFloat(prompt("Inserisci il primo numero"));
+console.log(num1);
+//secondo numero da inserire
+const num2 = parseFloat(prompt("Inserisci il secondo numero"));
+console.log(num2);
+
+// vedere quale numero è maggiore
+
+if (num1 > num2) {
+  console.log("Il numero maggiore è: " + num1);
+} else if (num2 > num1) {
+  console.log("Il numero maggiore è: " + num2);
+} else {
+  console.log("I numeri sono uguali.");
+}
